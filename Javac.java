1e8d8c47c65ba05ce00602b455e426aad302aa4c
Nithiyanila <182837985+Nila-7@users.noreply.github.com>
2026-07-18 20:33:14 +0530

    Add files via upload

---
+++ Javac.java
@@ -0,0 +1,13 @@
+package java_p;
+
+public class Javac {
+	public static void main(String args[]) {
+		System.out.println("Hello Nila");
+		
+		int age = 20;
+		System.out.println(age); 
+		
+		String depart_name = "CSE";
+		System.out.println(depart_name);
+	}      
+}
